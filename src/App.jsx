b0d6aa36b453cb446420a029349fe1750b6d0fda
adrian-amigues <adrian.amigues@gmail.com>
2020-03-24 19:06:24 +0100

    init

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const SexyDiv = styled.h1`
+  color: red;
+  border: solid 1px;
+`
+
+const App = () => {
+  return <SexyDiv>Hell World</SexyDiv>
+}
+
+export default App
